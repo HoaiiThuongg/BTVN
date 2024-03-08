4c03b4f51610a39df773f067336f605f60ba4d07
HoaiThuong <thuongthuong051103@gmail.com>
2024-03-08 16:07:05 +0700

    first

---
+++ Week1/b1.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int q,b,c;
+    cin >> q >> b >> c;
+    cout << q+b+c;
+    return 0;
+}
