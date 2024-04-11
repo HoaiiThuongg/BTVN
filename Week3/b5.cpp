3916ae6dbdac1710e803fcddf258f5040fe16207
HoaiThuong <162536593+HoaiiThuongg@users.noreply.github.com>
2024-04-11 13:24:32 +0700

    Update b5.cpp

--- Week3/b5.cpp
+++ Week3/b5.cpp
@@ -1,37 +1,14 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-/*
- * Complete the 'camelcase' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts STRING s as parameter.
- */
-
-string caesarCipher(string s, int k) {
-    for(int i = 0;i<s.length();i++)
-    {
-        if(isalpha(s[i]))
-        {
-            if(isupper(s[i]))
-            {
-                s[i] = (s[i] - 'A' + k)%26 +'A';
-            }
-            else 
-            {
-                s[i] = (s[i] - 'a' + k)%26 +'a';
-            }
-            
-        }
-
-    }
-    return s;
-}
-
-int main()
-{
-    string s;int k;
-    cin >> s>>k;
-    cout <<caesarCipher(s,k);
+void staircase(int n) {
+ for(int i = n-1;i>=0;i--)
+ {
+     for(int j = 1;j<=i;j++)
+     {
+         cout <<" ";
+     }
+     for(int j = 1;j<=n-i;j++)
+     {
+         cout <<"#";
+     }
+     cout << endl;
+ }
 }
