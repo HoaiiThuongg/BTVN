50061a6e0ea56272fb53985b1fe983b91f67cab7
HoaiThuong <162536593+HoaiiThuongg@users.noreply.github.com>
2024-04-11 13:57:52 +0700

    Update bai5.cpp

--- Week4/bai5.cpp
+++ Week4/bai5.cpp
@@ -1,53 +1,6 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-
-using namespace std;
-
-int getTotalX(vector<int> a, vector<int> b) {
-    int cnt = 0;
+int pageCount(int n, int p) {
+    int dau = p/2;
+    int cuoi = n/2 - p/2;
     
-    for(int i = a[a.size()-1];i<=b[0];i++)
-    {
-    	bool isYou = true;
-        for(int j = 0;j<a.size();j++)
-        {
-            if(i%a[j] !=0)
-            {
-            	isYou = false;
-                break;
-            }
-        }
-        
-        if(isYou)
-        {
-	        for(int j = 0;j<b.size();j++)
-	        {
-	            if(b[j]%i!=0)
-	            {
-	            	isYou = false;
-	                break;
-	            }
-	        }
-    	}
-        
-        if(isYou)
-        { 
-        	cnt++;
-		}
- 
-    }
-    return cnt;
-}
-
-
-int main()
-{
-    
-    vector<int> a = {2,6};
-    vector<int> b = {24,36};
-    
-    int res = getTotalX(a,b);
-    
-    cout << res;
+    return min (dau,cuoi);
 }
